f1fe0ceebbc59d821d03ab4586ca7c61c5c8524f
Serkan <68036775+SerkanBahtiyar@users.noreply.github.com>
2023-07-19 16:06:59 +0300

    cv project

---
+++ iletisim/iletisim/Controllers/DefaultController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using iletisim.Models.Entity;
+namespace iletisim.Controllers
+{
+    public class DefaultController : Controller
+    {
+        // GET: Default
+        iletisimEntities1 sb=new iletisimEntities1();
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpGet]
+        public PartialViewResult ilet()
+        {
+            return PartialView();
+        }
+        [HttpPost]
+        public PartialViewResult ilet(tbliletisim t)
+        {
+            sb.tbliletisim.Add(t);
+            sb.SaveChanges();
+            return PartialView();
+        }
+    }
+}
---
+++ iletisim/iletisim/Controllers/mesajlarController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using iletisim.Models.Entity;
+namespace iletisim.Controllers
+{
+    public class mesajlarController : Controller
+    {
+        // GET: mesajlar
+        iletisimEntities1 sb=new iletisimEntities1();
+        public ActionResult Index(string p)
+        {
+            var degerler = from x in sb.tbliletisim select x;
+            if (!string.IsNullOrEmpty(p))
+            {
+                degerler = degerler.Where(y => y.adSoyad.Contains(p));
+
+            }
+            return View(degerler.ToList());
+        }
+     
+    }
+}
